746379ae0e6da86def2b496eea06f9813a321dd4
nims0 <dc6058@nyu.edu>
2025-08-28 22:16:20 -0400

    upload

--- gettingStarted.py
+++ gettingStarted.py
@@ -16,7 +16,7 @@ def welcome_assignment_answers(question):
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-        answer = "SHA256:ov1Kf9GApAdTD4JkBoW5LMYJ+uPyVNch2Q4kNnrEZjU"
+        answer = "ea5dc4c58059831cc5452bb687b875a32b06a8c0b9ca1bd84f21880b860d2ebd"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
