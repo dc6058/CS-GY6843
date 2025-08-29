8d56d2a40b442a36046f18458b023bf3a2c0fa22
nims0 <dc6058@nyu.edu>
2025-08-28 22:03:54 -0400

    upload

--- gettingStarted.py
+++ gettingStarted.py
@@ -7,6 +7,8 @@ def welcome_assignment_answers(question):
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
+    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = ""
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
@@ -14,7 +16,7 @@ def welcome_assignment_answers(question):
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-        answer = ov1Kf9GApAdTD4JkBoW5LMYJ+uPyVNch2Q4kNnrEZjU
+        answer = "SHA256:ov1Kf9GApAdTD4JkBoW5LMYJ+uPyVNch2Q4kNnrEZjU"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
